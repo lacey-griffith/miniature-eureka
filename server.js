9908eeffbd053654a5815bdf5b1bbaa0a0fe1add
Lacey <lacey.griffith@g.austincc.edu>
2021-04-22 08:25:40 -0500

    get and post to server successfully

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 3001;
 const app = express()
 
 const fs = require('fs')
+const path = require('path')
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
@@ -15,18 +17,26 @@ const {notes} = require('./data/notes')
 
 
 
-
-
+function createNote(body, notesArr) {
+    const note = body;
+    notesArr.push(note)
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({notes: notesArr}, null, 2)
+    );
+    return note;
+}
 
 
 
 app.get('/api/notes', (req, res) => {
     res.json(notes)
 });
-// app.post('/notes', (req, res) => {
-//     req.body.id = notes.length.toString();
-//     res.json(req.body)
-// });
+app.post('/api/notes', (req, res) => {
+    const note = createNote(req.body, notes)
+    req.body.id = notes.length.toString();
+    res.json(note)
+});
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 })
