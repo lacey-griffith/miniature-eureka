fc7ca8dc8ad62c98371b38fc059a8f165e5a91ed
Lacey <lacey.griffith@g.austincc.edu>
2021-04-22 08:09:58 -0500

    add heroku

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const PORT = 3001;
+//process.env.PORT || 
+const app = express()
+
+const fs = require('fs')
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+const {notes} = require('./data/notes')
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/api/notes', (req, res) => {
+    // res.send('hell0')
+    res.json(notes)
+    console.log('getting somewhere')
+});
+// app.post('/notes', (req, res) => {
+//     req.body.id = notes.length.toString();
+//     res.json(req.body)
+// });
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+})
